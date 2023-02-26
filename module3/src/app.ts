ce8f97850e3df64df94b05c750a78d619c214e34
Alexander Radiuk <Aleksandr_Radyuk@epam.com>
2023-02-26 16:56:26 +0100

    #Module5 Add morgan logger

--- module3/src/app.ts
+++ module3/src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import { userRouter, usersRouter } from './controllers/user.controller';
 import { groupRouter, groupsRouter } from './controllers/group.controller';
 import { userGroupsRouter } from './controllers/user-group.controller';
+import morgan from 'morgan';
 import { Error } from 'sequelize';
 
 import * as dotenv from 'dotenv';
@@ -10,6 +11,10 @@ dotenv.config();
 
 const app = express();
 app.listen(process.env.DEV_PORT);
+
+morgan.token('body', (req: Request) => JSON.stringify(req.body));
+morgan.token('params', (req: Request) => Object.entries(req.params).map(([param, value]) => `${param}:${value}`).join(' '));
+app.use(morgan(':method :url :body :params - :response-time ms'));
 
 const appRouter = express.Router();
 appRouter.use('/user', userRouter);
@@ -21,5 +26,6 @@ app.use('', appRouter);
 
 // eslint-disable-next-line no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    req.params.error = err.message;
     res.status(StatusCodes.NOT_FOUND).json(err);
 });
